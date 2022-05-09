bad1d0ceedda35c925b1af0f5c01cf1fc309bd79
sooyeon <mingbbu@gmail.com>
2022-05-09 17:52:19 +0900

    1752

---
+++ P20220509/src/main/java/co/practice/PracticeDAO.java
@@ -0,0 +1,14 @@
+package co.practice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PracticeDAO {
+		
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	
+}
+
